fd384fbfcffeac17dd6c05455908a6f1521cd96b
Marcelo <marcelo.oc.machado@gmail.com>
2017-11-09 16:36:24 -0200

    Singleton to storage prints #9

---
+++ src/ppatosca/SingletonPrint.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppatosca;
+
+/**
+ *
+ * @author gtbavi
+ */
+public final class SingletonPrint {
+    private static SingletonPrint INSTANCE;
+    private static StringBuilder out = new StringBuilder();
+
+    private SingletonPrint() {
+    }
+    
+    
+    public static synchronized SingletonPrint getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new SingletonPrint();
+        }
+        return INSTANCE;
+    }
+    public void addString(String text){
+        out.append(text);
+    }
+    
+    public void print(){
+        System.out.println(out);
+    }
+    
+}
